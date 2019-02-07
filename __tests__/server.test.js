1d0d8d5a98cf6d9a555d69fe06807e08b3d65cc3
Miles Pratt <devornate@protonmail.com>
2019-02-07 12:46:49 -0500

    Update fake tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,4 +1,6 @@
+const sum = require("../sum");
+
 // fake test to get CI/CD going
 test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+  expect(sum()).toBe(3);
 });
---
+++ sum.js
@@ -0,0 +1,6 @@
+// temp file for fake tests
+function sum() {
+  return 1 + 2;
+}
+
+module.exports = sum;
